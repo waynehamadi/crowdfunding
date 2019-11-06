90ea416247a77b3d536880a2334dc3dddf85aa55
Merwane Hamadi <merwanehamadi@gmail.com>
2019-11-06 16:29:54 +0100

    17-activeadmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -62,12 +62,12 @@ ActiveAdmin.setup do |config|
   # method in a before filter of all controller actions to
   # ensure that there is a user with proper rights. You can use
   # CanCanAdapter or make your own. Please refer to documentation.
-  config.authorization_adapter = ActiveAdmin::CanCanAdapter
+  # config.authorization_adapter = ActiveAdmin::CanCanAdapter
 
   # In case you prefer Pundit over other solutions you can here pass
   # the name of default policy class. This policy will be used in every
   # case when Pundit is unable to find suitable policy.
-  config.pundit_default_policy = "ApplicationPolicy"
+  # config.pundit_default_policy = "MyDefaultPunditPolicy"
 
   # If you wish to maintain a separate set of Pundit policies for admin
   # resources, you may set a namespace here that Pundit will search
