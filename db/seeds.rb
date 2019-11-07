f82aafa44d57ca2342cdb8e65bd083bdc7dcb661
Merwane Hamadi <merwanehamadi@gmail.com>
2019-11-07 10:13:24 +0100

    19-seed-corrections

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.development?
-  if AdminUser.find_by_email('admin1@capsens.eu')
+  unless AdminUser.find_by_email('admin1@capsens.eu')
     AdminUser.create!(email: 'admin1@capsens.eu', password: 'password1', password_confirmation: 'password1') if Rails.env.development?
   end
-  if User.find_by_email('user1@capsens.eu')
-    User.create!(email: 'user1@capsens.eu', password: 'password1', password_confirmation: 'password1') if Rails.env.development?
+  unless AdminUser.find_by_email('admin2@capsens.eu')
+    AdminUser.create!(email: 'admin2@capsens.eu', password: 'password2', password_confirmation: 'password2') if Rails.env.development?
+  end
+  unless AdminUser.find_by_email('admin3@capsens.eu')
+    AdminUser.create!(email: 'admin3@capsens.eu', password: 'password3', password_confirmation: 'password3') if Rails.env.development?
+  end
+  unless User.find_by_email('user1@capsens.eu')
+    User.create!(first_name: "first_name1", last_name:"last_name2", birth_date:"12-01-1993", email: 'user1@capsens.eu', password: 'password1', password_confirmation: 'password1') if Rails.env.development?
   end
 end
