942c9bfdae69def49da092ddbeedd3bd7b4fb32b
Merwane Hamadi <merwanehamadi@gmail.com>
2019-11-07 09:54:10 +0100

    19-seed

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'devise'
 gem 'figaro'
 gem 'pg'
 gem 'dry-transaction'
+gem 'activeadmin'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: "users/registrations" }
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
