b5bf99d7d55513b70544e5bd28c79cd63342d529
Merwane Hamadi <merwanehamadi@gmail.com>
2019-11-08 15:22:52 +0100

    25-contribution-system-added-factory-bot-to-seed

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,2 +1,7 @@
 class Contribution < ApplicationRecord
+  belongs_to :user
+  belongs_to :project
+  belongs_to :counterpart
+  validates :amount_in_cents, :user, :project, :counterpart, presence: true
 end
+
