febe035fedea8df00a68d8f62b30a840cc59cd91
Merwane Hamadi <merwanehamadi@gmail.com>
2019-11-15 17:54:33 +0100

    34-facebook-connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,17 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :birthday, presence: true
   has_many :contributions, dependent: :destroy
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
 
   def self.from_omniauth(auth)
-    user = User.new()
-    user.email = auth.info.email
-    user.first_name = auth.info.first_name
-    user.last_name = auth.info.last_name
-    user.birthday = Date.strptime(auth.extra.raw_info.birthday, '%m/%d/%Y')
-    user.nationality = "FR"
-    user.country_of_residence = "FR"
-    user.provider = auth.provider
-    user.uid = auth.uid
-    return user
+    User.create(
+      email: auth.info.email,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      birthday: Date.strptime(auth.extra.raw_info.birthday, '%m/%d/%Y'),
+      nationality: 'FR',
+      country_of_residence: 'FR',
+      provider: auth.provider,
+      uid: auth.uid
+    )
   end
 end
